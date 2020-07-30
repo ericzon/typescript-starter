eb4836118e04636638ea213892dd60789dab10ac
Lisandro Gurman <Lisandro.gurman@axa.com>
2020-07-30 12:45:38 +0200

    feat: added integration test

---
+++ tests/features/support/steps.js
@@ -0,0 +1,16 @@
+// features/support/steps.js
+const { Given, When, Then } = require("cucumber");
+const { expect } = require("chai");
+
+Given("a variable set to {int}", function(number) {
+  this.setTo(number);
+  console.log("given step is working")
+});
+
+When("I increment the variable by {int}", function(number) {
+  this.incrementBy(number);
+});
+
+Then("the variable should contain {int}", function(number) {
+  expect(this.variable).to.eql(number);
+});
